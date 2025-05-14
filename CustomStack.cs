a24ce005381c20091f4980767cbbecfdc92e9142
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-14 08:03:14 -0700

    Add project files.

---
+++ CustomStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class CustomStack<T>
+{
+	private T[] data;
+	private int top;
+	private int capacity;
+	public CustomStack(int capacity)
+	{
+		top = -1;
+		data = new T[capacity];
+		this.capacity = capacity;
+	}
+	public void Push(T val)
+	{
+		if (top >= capacity - 1) 
+		{
+            throw new InvalidOperationException("Stack overflow!");
+        }
+		data[++top] = val;
+	}
+	public T Pop()
+	{
+		if (IsEmpty())
+		{
+            throw new InvalidOperationException("Stack underflow!");
+        }
+		return data[top--];
+	}
+	public T Peek()
+	{
+		if(IsEmpty())
+		{
+            throw new InvalidOperationException("Stack empty!");
+        }
+		return data[top];
+	}
+	public bool IsEmpty()
+	{
+		return top == -1;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿CustomStack<int> stack = new CustomStack<int>(10);
+stack.Push(87);
+stack.Push(123);
+stack.Push(2173);
+stack.Push(22);
+stack.Push(34);
+stack.Push(6459);
+stack.Push(7012);
+stack.Push(53);
+stack.Push(234);
+stack.Push(4675);
+stack.Pop();
+stack.Push(91);
+
+while(!stack.IsEmpty())
+{
+    Console.WriteLine(stack.Pop());
+}
